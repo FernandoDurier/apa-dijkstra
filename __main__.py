fc7a17219628c556c145b4eef20d38ac7ac26aee
FernandoDurier <fernando.durier@hotmail.com>
2018-06-23 18:49:41 -0300

    fixed imports in main

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
-from ./ordered_linked_list/ordered_linked_list import *
-from ./binheap/heap import *
-from ./logger/timer import *
-from ./dijkstra/closest_path import *
+from ordered_linked_list.ordered_linked_list import *
+from binheap.heap import *
+from logger.timer import *
+from dijkstra.closest_path import *
 
 def main():
     pass
